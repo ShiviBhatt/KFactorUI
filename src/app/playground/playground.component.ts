47bf3c4b60c55891904c084a639c29b477f6ea42
Azaz Akhtar <azaz.akhtar@powerschool.com>
2018-04-14 19:42:07 +0530

    created playground

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -7,9 +7,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaygroundComponent implements OnInit {
 
+  currentQuestion: KFQuestion;
+  selectedOption: KFOption;
+
   constructor() { }
 
-  ngOnInit() {
+  chooseOption(option: KFOption): void {
+    option.isLocked = true;
+    this.selectedOption = option;
+    setTimeout(() => {
+      option.isLocked = false;
+      option.isCorrect = false;
+    }, 2000);
   }
 
+  ngOnInit() {
+    this.currentQuestion = {
+      id: 'Q1',
+      description: 'Which one is an input device?',
+      options: [
+        {
+          id: 'OP1',
+          value: 'Monitor'
+        },
+        {
+          id: 'OP2',
+          value: 'Mouse'
+        },
+        {
+          id: 'OP3',
+          value: 'HDD'
+        },
+        {
+          id: 'OP4',
+          value: 'RAM'
+        }
+      ]
+    };
+  }
+
+}
+
+export interface KFQuestion {
+  id: string;
+  description: string;
+  options: KFOption[];
+  isCorrect?: boolean;
+}
+
+export interface KFOption {
+  id: string;
+  value: string;
+  isLocked?: boolean;
+  isCorrect?: boolean;
 }
